6dc8248d7e976dc26f59c8a502fb6fef96a94fc4
Helmi-Simplon <shan.tunisois@gmail.com>
2021-06-02 12:41:36 +0200

    display formView

---
+++ view/formView.php
@@ -0,0 +1,19 @@
+<form>
+<fieldset>
+    
+    <div class="row">
+    <div class="col form-group">
+      
+      <input type="text" class="form-control" id="pseudo" aria-describedby="pseudo" placeholder="Enter pseudo" >
+      
+    </div>
+    <div class="col form-group">
+      
+      <textarea class="form-control" id="exampleTextarea" rows="3" cols="33"></textarea>
+    </div>
+    
+  </div>   
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </fieldset>
+  
+  </form>
