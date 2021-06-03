c54c3ea132055e42aa28b96b40317e21b17dbb4c
Helmi-Simplon <shan.tunisois@gmail.com>
2021-06-03 09:19:13 +0200

    validator filds

--- model/controllers.php
+++ model/controllers.php
@@ -1,29 +1,44 @@
 <?php
 
 try {
+
     require 'dbconnect.php';
-    if($_POST==null || isset($query)){
+
+    if ($_POST == null || isset($query)) {
 
         $query1 = 'SELECT date, pseudo, content 
         FROM message order by id desc Limit 5';
-    
+
         $req = $dbh->query($query1);
         $req->setFetchMode(PDO::FETCH_ASSOC);
         $tab = $req->fetchAll();
         $req->closeCursor();
         return $tab;
     }
-else{
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                
-                $dbh->beginTransaction();
-        $query = 'INSERT INTO message (pseudo, content) VALUES ("'.$_POST["pseudo"].'", "'.$_POST["content"].'");';
-                $dbh->exec($query);
-                $dbh->commit();
-                header('Location: index.php');
-            }
+    
+    else {
+        
+        if (
+            $_POST["pseudo"] != null && strlen($_POST["pseudo"]) > 1 && preg_match("#^[A-Za-z '-]+$#", $_POST["pseudo"]) &&
+            $_POST["content"] != null && preg_match("#^[A-Za-z '-]+$#", $_POST["content"])
+        ) {
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-}catch (PDOException $e) {
+            $dbh->beginTransaction();
+            $query = 'INSERT INTO message (pseudo, content) VALUES ("' . $_POST["pseudo"] . '", "' . $_POST["content"] . '");';
+            $dbh->exec($query);
+            $dbh->commit();
+            header('Location: index.php');
+        } 
+        
+        else {
+            header('Location: index.php');
+            
+        }
+    }
+}
+
+catch (PDOException $e) {
     $dbh->rollBack();
     print "Erreur de requete!:" . $e->getMessage() . '<br>';
     die;
--- view/formView.php
+++ view/formView.php
@@ -1,19 +1,20 @@
 <form action="/" method="POST">
-<fieldset>
-    
+  <fieldset>
+
     <div class="row">
-    <div class="col-4 form-group">
-    <label for="pseudo" class="form-label mt-4" hidden>Pseudo</label>
-      <input type="text" name="pseudo" class="form-control" id="pseudo" aria-describedby="pseudo" placeholder="Entrer votre pseudo" >
-      
+      <div class="col-4 form-group">
+        <label for="pseudo" class="form-label mt-4" hidden>Pseudo</label>
+        <input type="text" name="pseudo" class="form-control" id="pseudo" aria-describedby="pseudo" placeholder="Entrer votre pseudo" required>
+
+      </div>
+      <div class="col-8 form-group">
+        <label for="message" class="form-label mt-4" hidden>message</label>
+        <textarea class="form-control" name="content" id="message" placeholder="Entrer votre message" rows="3" required></textarea>
+
+      </div>
+
     </div>
-    <div class="col-8 form-group">
-    <label for="message" class="form-label mt-4" hidden>message</label>
-      <textarea class="form-control" name="content" id="message" placeholder="Entrer votre message" rows="3" cols="33"></textarea>
-    </div>
-    
-  </div>   
-    <button type="submit" name = "submit"class="btn btn-primary">Envoyer</button>
+    <button type="submit" name="submit" class="btn btn-primary">Envoyer</button>
   </fieldset>
-  
-  </form>
+
+</form>
