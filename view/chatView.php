7cb5971ba7942a478c7655c1d9aaf6d82832cd78
Helmi-Simplon <shan.tunisois@gmail.com>
2021-06-04 11:38:03 +0200

    add link to editView.php

--- view/chatView.php
+++ view/chatView.php
@@ -12,7 +12,7 @@
                 <td class="col-2"><?php echo htmlspecialchars($row["date"])?></th>
                 <td class="col-2"><?php echo htmlspecialchars($row["pseudo"]) ?></td>
                 <td class="col-8"><?php echo htmlspecialchars($row["content"]) ?></td>
-
+                <td class="col-2"><a  href="/index?id=<?php echo htmlspecialchars($row["id"]).'&pseudo='.htmlspecialchars($row["pseudo"])?>"  style="color: white;"> <i class="fas fa-pen fa-xs" ></i></a> </td>
             </tr>
         <?php } ?>
     </tbody>
