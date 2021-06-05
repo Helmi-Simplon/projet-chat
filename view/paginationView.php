30421133b45551d66101087281ac6385f1a00a10
Helmi-Simplon <shan.tunisois@gmail.com>
2021-06-05 19:11:00 +0200

    creating a for loop to display the list of pagination

--- view/paginationView.php
+++ view/paginationView.php
@@ -1,7 +1,7 @@
 <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" style="margin-top:0px;margin-left:35%">
   <div class="btn-group me-2" role="group" aria-label="First group">
- 
-    <a href="" role="button" name="page" class="btn btn-secondary">1</a>
-
+      <?php for($i=1; $i <= $pagination["total_page"];$i++){ ?> 
+    <a href="index.php?page=<?= $i ?>" role="button" name="page" class="btn btn-secondary"><?= $i ?></a>
+    <?php } ?>
   </div>
   </div>
