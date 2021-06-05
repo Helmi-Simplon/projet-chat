135d81fb9921646a6b67a19f09fd1a3c78d32a7c
Helmi-Simplon <shan.tunisois@gmail.com>
2021-06-05 11:45:23 +0200

    add a condition to display editView.php in new page

--- view/default.php
+++ view/default.php
@@ -6,6 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://bootswatch.com/5/darkly/bootstrap.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  
   <title>Chat</title>
 </head>
 <body>
@@ -13,10 +15,19 @@
     <h1>Live chat Amazin</h1>
     
 <?php
-require 'chatView.php';
-require 'formView.php';
-if(isset($_GET["id"]))
-require 'editView.php';
+$_GET["id"]= isset($_GET["id"])?$_GET["id"]:NULL;
+$_GET["pseudo"]= isset($_GET["pseudo"])?$_GET["pseudo"]:NULL;
+$_GET["content"]= isset($_GET["content"])?$_GET["content"]:NULL;
+
+if(isset($_GET["id"])){
+  include 'view/editView.php';
+  }
+  else{
+  require 'chatView.php';
+  require 'formView.php';
+  }
+
+
 ?>
 
   </div>
