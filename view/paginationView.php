bf8434567db4fa17e0691e4ecd92798584ab616a
Helmi-Simplon <shan.tunisois@gmail.com>
2021-06-05 18:58:49 +0200

    create paginationView.php to display pagination list

---
+++ view/paginationView.php
@@ -0,0 +1,7 @@
+<div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" style="margin-top:0px;margin-left:35%">
+  <div class="btn-group me-2" role="group" aria-label="First group">
+ 
+    <a href="" role="button" name="page" class="btn btn-secondary">1</a>
+
+  </div>
+  </div>
